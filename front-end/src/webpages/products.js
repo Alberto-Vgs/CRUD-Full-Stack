6d261e5a03ee814ea9965a336fda73342fe2d990
alberto_vgs <alberto_b03@outlook.com>
2024-04-25 23:27:17 -0600

    fetch test

--- front-end/src/webpages/products.js
+++ front-end/src/webpages/products.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 const Product = () => {
+
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users/")
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    setIsLoaded(true);
+                    setUsers(data);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+      }, [])
+
 return(
         <div>
             <h1>Product Lists</h1>
+            <p>{users}</p>
         </div>
     );
 }
