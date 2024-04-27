59b39d776ab8da6f23356590c544f746bdce6bd9
alberto_vgs <alberto_b03@outlook.com>
2024-04-26 22:23:11 -0600

    erorr section

--- front-end/src/webpages/products.js
+++ front-end/src/webpages/products.js
@@ -20,6 +20,21 @@ const Product = () => {
             )
       }, [])
 
+      if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <ul>
+                {users.map(user => (
+                <li key={user.id}>
+                    {user.name} 
+                </li>
+                ))}
+            </ul>
+        );
+    }
 return(
         <div>
             <h1>Product Lists</h1>
