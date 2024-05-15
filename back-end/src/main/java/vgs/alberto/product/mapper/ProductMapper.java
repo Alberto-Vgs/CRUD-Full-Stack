5de5f75be1780a2bf2bec18306188e2e03499a65
alberto_vgs <alberto_b03@outlook.com>
2024-05-15 15:52:00 -0600

    product mappers

--- back-end/src/main/java/vgs/alberto/product/mapper/ProductMapper.java
+++ back-end/src/main/java/vgs/alberto/product/mapper/ProductMapper.java
@@ -7,10 +7,21 @@ public class ProductMapper {
 
     public static Product mapToProduct(ProductDTO dto) {
         Product product = new Product();
-        product.setProductId(dto.getProductId());
-        product.setProductName(dto.getProductName());
-        product.setProductPrice(dto.getProductPrice());
-        product.setProductQuantity(dto.getProductQuantity());
+        product.setProductId(dto.getKey());
+        product.setProductName(dto.getName());
+        product.setProductPrice(dto.getPrice());
+        product.setProductQuantity(dto.getQuantity());
+        product.setProductStatus(dto.getStatus());
         return product;
+    }
+
+    public static ProductDTO mapToDTO(Product prd) {
+        ProductDTO dto = new ProductDTO();
+        dto.setKey(prd.getProductId());
+        dto.setName(prd.getProductName());
+        dto.setPrice(prd.getProductPrice());
+        dto.setQuantity(prd.getProductQuantity());
+        dto.setStatus(prd.getProductStatus());
+        return dto;
     }
 }
